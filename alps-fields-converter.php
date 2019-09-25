9466fe9657b717e0aa6ccf8a3891194ee419104f
David Eleuterius <davideleuterius@gmail.com>
2019-09-25 12:23:43 -0500

    update options - v3

--- alps-fields-converter.php
+++ alps-fields-converter.php
@@ -70,7 +70,7 @@ function alps_convert_fields() {
             elseif ( in_array( $opt_key,  $option_image_fields ) ) {
                add_option( '_data_check_' .$opt_key, $opt_val );
                // ADDRESSING WEIRD BUG FOR SINGLE ARRAY VALUES - MOSTLY IMAGES
-              $img_data = @unserialize( $opt_val );
+              $img_data = unserialize( $opt_val );
               if ($img_data !== false) {
                 add_option( '_data_good_' .$opt_key, $img_data[0] );
               }
